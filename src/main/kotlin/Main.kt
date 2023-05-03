5399eae2036cdf898cdbf63070671f84dfea40b1
gogerch <gogerch@gmail.com>
2023-05-03 14:02:56 +0300

    val

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    //println("Hello World!")
+    //это комментарий
+
+    val numberOfOrders: Int = 34
+
+    var notificationText: String = "Помой жопу"
+
+    println(numberOfOrders)
+    println(notificationText)
+
+    notificationText = "Жопу больше не мой"
+    println(notificationText)
+
+    //целочисленные
+    val intNum1: Int = -2147483648 // 32bit
+    val IntNum2: Int = 2147483647 // 32bit
+    val longNum: Long // 64bit
+    val shortNum: Short // 16bit
+    val byteNum: Byte // 8bit
+    val unsignetNum: UInt = 2147483647u // 32bit
+
+    //вещественные
+    val doubleNum: Double = 234.44 // 64bit
+    van floatNum: Float = 232.5f // 32bit
+
+    // строковые
+    val stringValue: String = "Это строка"
+    val charValue: Char = '1'
+
+    // логические
+    val trueValue: Boolean = true
+    val falseValue: Boolean = false
+}
